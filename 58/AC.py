2b5bd756964081290f5c0467eeddb27284893829
korsa351 <korsa351@gmail.com>
2021-10-25 15:15:15 +0300

    all tests is OK

--- 58/AC.py
+++ 58/AC.py
@@ -1,26 +1,22 @@
-def simplenumbers(g, n):
-    numbrs = []
-    for x in range(g, n+1):
-        f = 2
-        while x % f != 0:
-            f += 1
-        if f == x:
-            numbrs.append(x)
-    return numbrs
+def IsPrime(n):
+    d = 2
+    while d * d <= n and n % d != 0:
+        d += 1
+    return d * d > n
 
 
 a = int(input())
-if a <= 1:
+perfect_numbers = []
+if a <= 5:
     print(0)
     exit()
-perfect_numbers = []
-Array_simple_numbers = simplenumbers(2, 100)
-for i in Array_simple_numbers:
-    perfect_number = 2**(i-1)*(2**i-1)
-    if perfect_number <= a:
-        perfect_numbers.append(perfect_number)
-    else:
-        pass
+for i in range(2, 101):
+    if IsPrime(2**i-1):
+        perfect_number = 2**(i-1)*(2**i-1)
+        if perfect_number <= a:
+            perfect_numbers.append(perfect_number)
+        else:
+            break
 if perfect_numbers:
     print(*perfect_numbers)
 else:
