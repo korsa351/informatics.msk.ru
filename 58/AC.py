e13b3d15a1e660f6bd6637d359bbf944a1bc67d2
korsa351 <korsa351@gmail.com>
2021-10-25 03:42:18 +0300

    16 tests is OK

--- 58/AC.py
+++ 58/AC.py
@@ -1,52 +1,27 @@
-import collections
-import itertools
-
-
-def get_prime_divisors(n):
-    i = 2
-    while i * i <= n:
-        if n % i == 0:
-            n /= i
-            yield i
-        else:
-            i += 1
-
-    if n > 1:
-        yield n
-
-
-def calc_product(iterable):
-    acc = 1
-    for i in iterable:
-        acc *= i
-    return acc
-
-
-def get_all_divisors(n):
-    primes = get_prime_divisors(n)
-
-    primes_counted = collections.Counter(primes)
-
-    divisors_exponentiated = [
-        [int(div) ** i for i in range(count + 1)]
-        for div, count in primes_counted.items()
-    ]
-
-    for prime_exp_combination in itertools.product(*divisors_exponentiated):
-        yield calc_product(prime_exp_combination)
+def simplenumbers(g, n):
+    numbrs = []
+    for x in range(g, n+1):
+        f = 2
+        while x % f != 0:
+            f += 1
+        if f == x:
+            numbrs.append(x)
+    return numbrs
 
 
 a = int(input())
-total = []
-for g in range(1, a + 1):
-    D = list(get_all_divisors(g))
-    try:
-        D.remove(g)
-    except ValueError:
+if a <= 1:
+    print(0)
+    exit()
+perfect_numbers = []
+Array_simple_numbers = simplenumbers(2, 100)
+for i in Array_simple_numbers:
+    perfect_number = 2**(i-1)*(2**i-1)
+    if perfect_number <= a:
+        perfect_numbers.append(perfect_number)
+    else:
         pass
-    if sum(D) == g:
-        total.append(g)
-if total:
-    print(*total)
+if perfect_numbers:
+    print(*perfect_numbers)
 else:
     print(0)
